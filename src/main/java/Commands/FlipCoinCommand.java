31be6b0df7214bd98bb3064da5c6063e83aa72ca
Vexrax <joshuaf_111@live.com>
2019-02-18 13:33:01 -0500

    Update flipcoin command so that it just shows what side it landed on

--- src/main/java/Commands/FlipCoinCommand.java
+++ src/main/java/Commands/FlipCoinCommand.java
@@ -4,28 +4,22 @@ import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
+
 public class FlipCoinCommand implements Command
 {
 
 	public boolean called(String[] args, MessageReceivedEvent e) 
 	{
-		if(args[0].equals("heads") || args[0].equals("tails"))
-			return true;
-		return false;
+		return true;
 	}
 
 	public void action(String[] args, MessageReceivedEvent e) 
 	{
         MessageChannel objChannel = e.getChannel();
         User objUser = e.getAuthor();   
-
+		String[] stringdict = {"Heads", "Tails"};
     	int Random = (int)(Math.random()*2);
-        if (Random == 1 && args[0].equals("heads") || Random == 0 && args[0].equals("tails"))
-        {
-        	objChannel.sendMessage(objUser.getName() + " called the side CORRECTLY").queue();
-        	return;
-        }
-        objChannel.sendMessage(objUser.getName() + " called the side INCORRECTLY").queue();
+		objChannel.sendMessage("The Coin Landed on " + stringdict[Random]).queue();
         return;
 	}
 
